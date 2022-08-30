9f9124a4ae282cfd593b99d13094e5f34a7d8da0
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-08-29 21:09:07 -0500

    if else and switch

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -307,3 +307,41 @@ if (dolphinAvg > koalaAvg && dolphinAvg >= 100) {
     console.log("Neither team met 100 point requirement, no trophy is awarded!")
 }
 */
+const day = "sunday";
+switch (day) {
+    case "monday":
+        console.log("Plan course structure");
+        console.log("Go to coding meetup");
+        break;
+    case "tuesday":
+        console.log("Prepare Theory videos")
+        break;
+    case "wednesday":
+    case "thursday":
+        console.log("Write code examples");
+        break;
+    case "friday":
+        console.log("record videos")
+        break;
+    case "saturday":
+    case "sunday":
+        console.log("enjoy the weekend!");
+        break;
+    default:
+        console.log("not a valid day")
+}
+
+if (day === "monday") {
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+    console.log("Prepare Theory videos")
+} else if (day === "wednesday" || day === "thursday") {
+    console.log("Write code examples");
+} else if (day === "friday") {
+    console.log("record videos")
+} else if (day === "saturday" || day === "sunday") {
+    console.log("enjoy the weekend!");
+} else {
+    console.log("not a valid day")
+}
