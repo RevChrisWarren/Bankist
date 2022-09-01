444e0b91b04afe6d03644e24f67d38639d9b7d16
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-08-31 20:53:12 -0500

    on second grouping of fundamentals

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,17 @@
+"use strict";
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive!");
+//reserved for possible future use
+//const interface = "audio";
+//const private = "home";
+*/
+
+function logger() {
+    console.log('My name is Chris');
+}
+
+logger();
