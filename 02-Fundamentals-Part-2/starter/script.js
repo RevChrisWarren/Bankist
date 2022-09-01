f4ea445c1affc25a966619a2875d169afe04eb6d
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-08-31 21:48:46 -0500

    function expressions declarations and arrow functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -8,7 +8,7 @@ if (hasDriversLicense) console.log("I can drive!");
 //reserved for possible future use
 //const interface = "audio";
 //const private = "home";
-*/
+
 
 function logger() {
     console.log('My name is Chris');
@@ -29,4 +29,50 @@ console.log(appleJuice);
 console.log(fruitProcessor(5, 0));
 
 const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice)
+console.log(appleOrangeJuice);
+
+//function declaration
+function calcAge1(birthYear) {
+    return 2022 - birthYear;
+};
+
+const chrisAge1 = calcAge1(1974);
+console.log(chrisAge1);
+
+//function expression
+const calcAge2 = function (birthYear) {
+    return 2022 - birthYear;
+}
+const chrisAge2 = calcAge2(1975);
+console.log(chrisAge2);
+
+
+//Arrow function
+
+const calcAge3 = birthYear => 2022 - birthYear;
+const chrisAge3 = calcAge3(1974)
+console.log(chrisAge3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2022 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years`
+}
+
+console.log(yearsUntilRetirement(1974, "Chris"))
+console.log(yearsUntilRetirement(1971, "Glenn"));
+*/
+
+function fruitCutter(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = fruitCutter(apples);
+    const orangePieces = fruitCutter(oranges);
+    // console.log(apples, oranges);
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`
+    return juice;
+}
+const newJuice = fruitProcessor(2, 3)
+console.log(newJuice)
