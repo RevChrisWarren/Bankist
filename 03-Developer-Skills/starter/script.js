f594eb325826471fc3f55c83bec8a4e6ab5ca203
RevChrisWarren <chriswarren37129@gmail.com>
2022-09-05 21:38:39 -0500

    coding challenge for section 3 with teach solution

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -94,14 +94,30 @@ const calcTempAmplitudeBug = function (t1, t2) {
 const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 3, 5])
 console.log(amplitudeBug)
 */
-
+//my solution
 const arr1 = [17, 21, 23]
 const arr2 = [12, 5, -5, 0, 4]
 
 const printForecast = function (arr) {
     for (let i = 0; i < arr.length; i++) {
-        console.log(`... ${arr[i]}°C in ` + (i + 1) + ` days`)
+        console.log(`... ${arr[i]}°C in ${i + 1} days`)
     }
 }
 
 printForecast(arr2)
+
+//teachsolution
+
+const data1 = [17, 21, 23]
+const data2 = [12, 5, -5, 0, 4]
+
+const printForecastTeacher = function (arr) {
+    let str = ''
+    for (let i = 0; i < arr.length; i++) {
+        str = str + `${arr[i]}°C in ${i + 1} days ... `
+
+    }
+    console.log('... ' + str)
+}
+
+printForecastTeacher(data2)
