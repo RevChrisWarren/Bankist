8e737eaebe2f3910ac46b7dcae071a62b615af3d
RevChrisWarren <chriswarren37129@gmail.com>
2022-09-07 21:31:01 -0500

    more DOM manipulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,3 +2,9 @@
 
 
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = "🎉 Correct Number!"
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 15;
+document.querySelector('.guess').value = 12;
+console.log(document.querySelector('.guess').value)
