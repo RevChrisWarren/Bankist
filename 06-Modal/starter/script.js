ad863c8d9a586083748c20946ace14d6646f92b0
RevChrisWarren <chriswarren37129@gmail.com>
2022-09-09 18:08:33 -0500

    modal project complete

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -2,9 +2,28 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const closeModal = document.querySelector('.close-modal');
-const showModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
 
-console.log(showModal);
-for (let i = 0; i < showModal.length; i++)
-    console.log(showModal[i].textContent);
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+const openModal = function () {
+    console.log("button clicked");
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+
+for (let i = 0; i < btnShowModal.length; i++)
+    btnShowModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
