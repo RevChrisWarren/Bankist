e4430a40f42a745d987bf1806830ee6cbedc5ac4
RevChrisWarren <chriswarren37129@gmail.com>
2022-09-09 17:38:37 -0500

    modals

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+const showModal = document.querySelectorAll('.show-modal');
+
+console.log(showModal);
+for (let i = 0; i < showModal.length; i++)
+    console.log(showModal[i].textContent);
