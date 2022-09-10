cd19fdee568d74cd6791c246df47678ab3de5f2f
RevChrisWarren <chriswarren37129@gmail.com>
2022-09-10 18:28:20 -0500

    finished game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -12,14 +12,24 @@ const currentScore1El = document.getElementById('current--1');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1')
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    currentScore0El.textContent = 0;
+    currentScore1El.textContent = 0;
+    diceEl.classList.add('hidden');
+}
+init();
 
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -59,7 +69,7 @@ btnHold.addEventListener('click', function () {
         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
         //2. check if score is >= 100
         //finish game
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 100) {
             //Finish the game
             playing = false;
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
@@ -72,14 +82,4 @@ btnHold.addEventListener('click', function () {
     }
 })
 
-btnNew.addEventListener('click', function () {
-    document.querySelector(`.player--0`).classList.remove('player--winner');
-    document.querySelector(`.player--1`).classList.remove('player--winner');
-    document.querySelector(`.player--0`).classList.add('player--active');
-    document.querySelector(`.player--1`).classList.remove('player--active');
-    document.getElementById(`score--0`).textContent = 0;
-    document.getElementById(`score--1`).textContent = 0;
-    document.getElementById(`current--0`).textContent = 0;
-    document.getElementById(`current--1`).textContent = 0;
-    playing = true;
-})
+btnNew.addEventListener('click', init);
