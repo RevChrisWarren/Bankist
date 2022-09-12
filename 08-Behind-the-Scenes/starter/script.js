d22a902e1907b06e7ba45cc1779b8290405faefe
RevChrisWarren <chriswarren37129@gmail.com>
2022-09-11 21:35:42 -0500

    weird example

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+//Random code used as example. Doesn't really make sense.
+const name = 'Jonas'
+
+const first = () => {
+    let a = 1;
+    const b = second(7, 9);
+    a = a + b;
+    console.log(a);
+    console.log(b);
+    return a;
+
+}
+
+function second(x, y) {
+    var c = 2;
+    return c;
+}
+
+const x = first();
+//console.log(a);
+//console.log(b);
+console.log(x);
