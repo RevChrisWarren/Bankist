70a7ceb39772153498fd234fb721e3b97da028bb
RevChrisWarren <chriswarren37129@gmail.com>
2022-09-12 19:56:32 -0500

    scoping

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 //Random code used as example. Doesn't really make sense.
 const name = 'Jonas'
 
@@ -22,3 +22,24 @@ const x = first();
 //console.log(a);
 //console.log(b);
 console.log(x);
+*/
+
+function calcAge(birthYear) {
+    const age = 2037 - birthYear;
+    console.log(firstName); //There because it is in global scope
+
+    function printAge() {
+        const output = `${firstName}, you are ${age}, born in ${birthYear}`
+        console.log(output);
+
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            const str = `Oh, and you're a millennial, ${firstName}`
+            console.log(str);
+        }
+    }
+    printAge();
+    return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
