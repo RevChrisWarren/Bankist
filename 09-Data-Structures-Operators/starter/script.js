1d4758f1b89d002ed671c2bddb141f1c07f0d934
RevChrisWarren <chriswarren37129@gmail.com>
2022-09-15 16:51:36 -0500

    rest

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -4,6 +4,7 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+  */
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -34,9 +35,14 @@ const restaurant = {
   },
   orderPasta: function (ing1, ing2, ing3) {
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}!`);
+  },
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient, otherIngredients);
   }
 };
 
+restaurant.orderPizza('mushrooms', 'onion', 'spinach')
+/*
 // const ingredients =
 //   [prompt("Let's make some pasta! Ingredient 1?"),
 //   prompt("Ingredient 2?"),
@@ -155,7 +161,37 @@ console.log(i, j, k, l);
 // default value
 const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r);
+
+//For DESTRUCTURING
+//rest pattern used on left hand side of =
+
+const arr = [1, 2, ...[3, 4, 5]];
+
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu]
+//does not include skipped elements--Rest element must be last element
+console.log(pizza, risotto, otherFood);
+
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+//Rest operator in functions
+const add = function (...numbers) {
+  console.log(numbers);
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i]
+    console.log(sum);
+  }
+
+}
+add(2, 3);
+add(5, 3, 7, 2)
+add(8, 2, 5, 3, 2, 1, 4)
+const x = [23, 4, 5];
+add(...x);
+
 */
-
-//rest pattern
-
