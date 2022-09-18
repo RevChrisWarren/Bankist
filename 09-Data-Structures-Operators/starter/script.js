5f740b3aac17badd5af0c3d27898d94ae88b674c
RevChrisWarren <chriswarren37129@gmail.com>
2022-09-17 19:41:22 -0500

    maps data structure

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -518,7 +518,7 @@ for (const [team, odd] of Object.entries(game.odds)) {
   console.log(`Odd of ${teamStr}: ${odd}`);
 }
 
-*/
+
 
 //Sets = collection of unique values
 
@@ -550,6 +550,41 @@ console.log(staffUnique);
 console.log(new Set(staff).size);
 
 console.log(new Set('ChrisWarren').size);
+
+*/
+
+//Maps= data structire used to map values to keys.Keys can be of any type.
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy')
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, "We are closed")
+
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+
+rest.delete(2);
+//rest.clear();
+const arr = [1, 2]
+rest.set(arr, 'Test')
+rest.set(document.querySelector('h1'), 'Heading')
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
 
 
 
