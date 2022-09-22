74297a101461cf42827152d4d1a3c16553bb1efb
RevChrisWarren <chriswarren37129@gmail.com>
2022-09-21 21:28:14 -0500

    value vs reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const bookings = []
 
 const createBooking = function (flightNum, numPassengers = 1, price = 199 * numPassengers) {
@@ -20,3 +20,35 @@ createBooking('LH123');
 createBooking('LH123', 45, 325);
 createBooking('LH123', 4);
 createBooking('LH123', undefined, 325);
+
+const flight = 'LH234';
+const chris = {
+    name: 'Chris Warren',
+    passport: 246398126
+}
+
+const checkIn = function (flightNumber, passenger) {
+    flightNumber = 'Lh999';
+    passenger.name = 'Mr.' + passenger.name
+    if (passenger.passport === 246398126) {
+        alert('Checked In')
+    } else {
+        alert('Wrong Passport Number')
+    }
+}
+
+// checkIn(flight, chris)
+// console.log(flight, chris);
+
+// //same as doing this
+// const flightNumber = flight;
+// const passenger = chris;
+
+const newPassport = function (person) {
+    person.passport = Math.trunc(Math.random() * 10000000000)
+}
+
+newPassport(chris)
+checkIn(flight, chris)
+console.log(chris);
+*/
