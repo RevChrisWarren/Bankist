53a19db0c614f574ef8539ff92b5f4a0607921ad
RevChrisWarren <chriswarren37129@gmail.com>
2022-09-23 21:41:26 -0500

    challenge with teachers answer

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -200,8 +200,12 @@ const poll = {
     options: ['0: Javascript', '1: Python', '2: Rust', '3: C++'],
     answers: new Array(4).fill(0),
     registerNewAnswer() {
-        const answer = prompt(`${this.question}\n ${this.options[0]}\n ${this.options[1]}\n ${this.options[2]}\n ${this.options[3]}`)
+        const answer = prompt(`${this.question}\n ${this.options[0]}\n ${this.options[1]}\n ${this.options[2]}\n ${this.options[3]}\n(Write Option Number)`)
+        //const answer = Number (prompt(`${this.question}\n${this.options.join('\n\')}\n(Write option number)`));
         console.log(answer);
+        //typeof answer === 'number' && answer < this.answers.length
+        //&& this.answers(answer)++
+
         if (answer === '0') {
             this.answers[0]++
         } else if (answer === '1') {
@@ -216,6 +220,8 @@ const poll = {
         console.log(this.answers);
         displayResult(this.answers)
     }
+    //displayResults(type = 'array') { if (type === 'array') {console.log(this.answers)}
+    //else if(type==='string') { console.log(`Poll results are $(this.answers.join(', ')}`)
 
 }
 
@@ -228,3 +234,6 @@ const displayResult = function (type) {
 }
 displayResult([5, 2, 3])
 displayResult([1, 5, 3, 9, 6, 1])
+
+
+//poll.displayResults.call({answers: [5,2,3] })
