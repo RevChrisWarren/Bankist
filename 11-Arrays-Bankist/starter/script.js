0e24ea7017e6699be565b7e63902dc3bdc968771
RevChrisWarren <chriswarren37129@gmail.com>
2022-09-25 16:38:34 -0500

    starting bankify

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -65,13 +65,13 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-// const currencies = new Map([
-//   ['USD', 'United States dollar'],
-//   ['EUR', 'Euro'],
-//   ['GBP', 'Pound sterling'],
-// ]);
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 /*
@@ -121,7 +121,7 @@ console.log(arr.slice(-1)[0]);
 console.log(arr.at(-1));
 
 console.log('chris'.at(0));
-*/
+
 
 //LOOPING OVER ARRAYS USING FOR EACH METHOD
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
@@ -165,3 +165,4 @@ console.log(currenciesUnique);
 currenciesUnique.forEach(function (value, key, set) {
   console.log(`${key}: ${value}: ${set}`);
 })
+*/
